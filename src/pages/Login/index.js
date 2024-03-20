550eb8268a14c39ea2e5daf632404dc04cd96f52
unknown <fuweiheng>
2024-03-20 19:26:16 +0800

    this is new version#

--- src/pages/Login/index.js
+++ src/pages/Login/index.js
@@ -1,9 +1,54 @@
-const Login = ()=>{
-    return (
-        <div>
-            Login
-        </div>
-    )
+
+import './index.scss'
+import { Card, Form, Input, Button } from 'antd'
+import logo from '@/assets/logo.png'
+
+const Login = () => {
+    const onFinish = (values)=>{
+        console.log(values)
+    }
+    const onFinishFailed = ()=>{
+        alert('请输入正确的验证码')
+    }
+  return (
+    <div className="login">
+      <Card className="login-container">
+        <img className="login-logo" src={logo} alt="" />
+        {/* 登录表单 */}
+        <Form 
+        validateTrigger='onBlur'
+        onFinish={onFinish}
+        onFinishFailed={onFinishFailed}
+        >
+          <Form.Item
+          name="mobile"
+          rules={[
+            { 
+                required: true, message: '请输入手机号!' 
+            },
+            {
+                pattern: /^1[3-9]\d{9}$/,
+                message: '请输入正确手机号格式!' 
+            }    
+        ]}
+          >
+            <Input size="large" placeholder="请输入手机号"/>
+          </Form.Item>
+          <Form.Item
+          name="varifycode"
+          rules={[{ required: true, message: '请输入验证码!' }]}
+          >
+            <Input size="large" placeholder="请输入验证码" />
+          </Form.Item>
+          <Form.Item>
+            <Button type="primary" htmlType="submit" size="large" block>
+              登录
+            </Button>
+          </Form.Item>
+        </Form>
+      </Card>
+    </div>
+  )
 }
 
 export default Login
